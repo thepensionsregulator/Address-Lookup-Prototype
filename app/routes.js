e165ab651b78424b882f51b66fe839228277ed1b
HarryAmmon <harry.ammon@tpr.gov.uk>
2023-09-08 11:54:51 +0100

    Intergration with UPRN OS API

--- app/routes.js
+++ app/routes.js
@@ -6,5 +6,5 @@
 const govukPrototypeKit = require('govuk-prototype-kit');
 const addressLookupRouter = require('./routers/addressLookupRouter');
 const router = govukPrototypeKit.requests.setupRouter()
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 router.use("/address-lookup", addressLookupRouter);
